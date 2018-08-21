d959cd571725c80d01e3a9b79cd5be543e8c3515
ryan han <rhep0820@gmail.com>
2018-08-21 14:14:36 +0900

    VO로 Controller 코드 재작성

--- src/main/java/com/example/demo/controller/BotController.java
+++ src/main/java/com/example/demo/controller/BotController.java
@@ -8,27 +8,123 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.vo.KeyboardVO;
+import com.example.demo.vo.MessageButtonVO;
+import com.example.demo.vo.MessageVO;
 import com.example.demo.vo.PhotoVO;
+import com.example.demo.vo.RequestMessageVO;
+import com.example.demo.vo.ResponseMessageVO;
 
 import net.minidev.json.JSONObject;
 
 @RestController
 public class BotController {
 	
-	
+//========== VO로 구현 ==========	
+
 	@RequestMapping(value = "/keyboard", method = RequestMethod.GET)
 	public KeyboardVO keyboard() {
-		
-//========== VO로 구현 ==========
 		
 		KeyboardVO keyboard = new KeyboardVO
 				(new String[] {"챗봇이랑 대화하기", "공지사항 보기", "FAQ 보기", "1:1 문의하기"});
 		
 		return keyboard;		
 	}
+	
+	@RequestMapping(value = "/message", method = RequestMethod.POST)
+	public ResponseMessageVO message(@RequestBody RequestMessageVO vo) {
+		
+		ResponseMessageVO res_vo = new ResponseMessageVO();
+		MessageVO mes_vo = new MessageVO();
+		String command = vo.getContent();
+		
+		
+		if(command.equals("메뉴")) {
+			
+			mes_vo.setText("메뉴에서 명령을 선택해주세요~");
+			KeyboardVO keyboard = new KeyboardVO(new String[] 
+					{"챗봇이랑 대화하기", "공지사항 보기", "FAQ 보기", "1:1 문의하기"});
+			res_vo.setKeyboard(keyboard);
+		}
+		else if(command.equals("1:1 문의하기")) {
+			
+			MessageButtonVO messageButton = new MessageButtonVO();
+			messageButton.setLabel("1:1 문의하러 가기");
+			messageButton.setUrl("https://www.funda.kr/v2/contact");
+			mes_vo.setMessage_button(messageButton);
+			mes_vo.setText("안녕하세요 고객님, 아래의 URL을 클릭하여 1:1 문의사항을 작성해주세요. (메뉴 다시보기 = '메뉴' 입력!)");
+		}
+		else if(command.equals("공지사항")) {
+			
+			MessageButtonVO messageButton = new MessageButtonVO();
+			messageButton.setLabel("공지사항 보러 가기");
+			messageButton.setUrl("https://www.funda.kr/v2/news?mode=story");
+			mes_vo.setMessage_button(messageButton);
+			mes_vo.setText("공지사항을 여기에 뿌려주는 기능을 차차 구현하려 합니다. "
+					+ "지금은 아래 링크를 클릭하여 공지사항을 확인해주세요ㅎ (메뉴 다시보기 = '메뉴' 입력!)");
+		}
+		else if(command.equals("FAQ 보기")) {
+			
+			mes_vo.setText("FAQ 범위를 선택해주세요");
+			KeyboardVO keyboard = new KeyboardVO(new String[] {"공통", "대출자", "투자자", "취소"});
+			res_vo.setKeyboard(keyboard);
+		}
+		else if(command.equals("공통")) {
+			
+			MessageButtonVO messageButton = new MessageButtonVO();
+			messageButton.setLabel("공통 FAQ 확인하기");
+			messageButton.setUrl("https://www.funda.kr/v2/faq");
+			mes_vo.setMessage_button(messageButton);
+			mes_vo.setText("아래 링크를 통해 공통 FAQ를 확인하세요! (메뉴 다시보기 = '메뉴' 입력!)");
+		}
+		else if(command.equals("대출자")) {
+			
+			MessageButtonVO messageButton = new MessageButtonVO();
+			messageButton.setLabel("대출자 FAQ 확인하기");
+			messageButton.setUrl("https://www.funda.kr/v2/faq");
+			mes_vo.setMessage_button(messageButton);
+			mes_vo.setText("아래 링크를 통해 대출자 FAQ를 확인하세요! (메뉴 다시보기 = '메뉴' 입력!)");
+		}
+		else if(command.equals("투자자")) {
+	
+			MessageButtonVO messageButton = new MessageButtonVO();
+			messageButton.setLabel("투자자 FAQ 확인하기");
+			messageButton.setUrl("https://www.funda.kr/v2/faq");
+			mes_vo.setMessage_button(messageButton);
+			mes_vo.setText("아래 링크를 통해 투자자 FAQ를 확인하세요! (메뉴 다시보기 = '메뉴' 입력!)");
+		}
+		else if(command.equals("취소")) {
+			
+			mes_vo.setText("메뉴에서 명령을 선택해주세요~");
+			KeyboardVO keyboard = new KeyboardVO(new String[] 
+					{"챗봇이랑 대화하기", "공지사항 보기", "FAQ 보기", "1:1 문의하기"});
+			res_vo.setKeyboard(keyboard);
+		}
+		else if(command.equals("챗봇이랑 대화하기")){
+			
+			PhotoVO photo = new PhotoVO();
+			photo.setUrl("http://www.businesscomputingworld.co.uk/wp-content/uploads/2018/01/Chatbot.jpg");
+			photo.setHeight(540);
+			photo.setWidth(427);
+			mes_vo.setPhoto(photo);
+			mes_vo.setText("(하트뿅) 저와 대화를 나눠볼까용? 아직은 매우 멍청하답니다ㅎㅎ (메뉴 다시보기 = '메뉴' 입력!)");
+		}
+		else if(command.contains("안녕")){
+			mes_vo.setText("안녕하세요ㅎㅎ");
+		}
+		else {
+			mes_vo.setText("아직 구현하지 않은 명령어입니다. Ryan에게 문의하세요~");
+		}
+		
+		res_vo.setMessage(mes_vo);
+		return res_vo;
+	}
+}	
 //========== JSON 객체로 구현  ==========
 		
-		/*JSONObject joBtn = new JSONObject();
+	/*@RequestMapping(value = "/keyboard", method = RequestMethod.GET)
+	public String keyboard() {
+	
+		JSONObject joBtn = new JSONObject();
 		ArrayList<String> btns = new ArrayList<>();
 		
 		btns.add("챗봇이랑 대화하기");
@@ -39,7 +135,7 @@ public class BotController {
 		joBtn.put("type", "buttons");
 		joBtn.put("buttons", btns);
 		
-		return joBtn.toJSONString();*/
+		return joBtn.toJSONString();
 
 	
 	
@@ -135,10 +231,10 @@ public class BotController {
 			photo.put("height", 427);
 			joText.put("photo", photo);
 			
-			/*PhotoVO photo = new PhotoVO();
+			PhotoVO photo = new PhotoVO();
 			photo.setUrl("https://www.channelsight.com/wp-content/uploads/2018/01/what-chatbot.png");
 			photo.setWidth(481);
-			photo.setHeight(395);*/
+			photo.setHeight(395);
 			
 		}
 		else if(content.contains("안녕")) {
@@ -165,4 +261,4 @@ public class BotController {
 		
 		return joRes.toJSONString();
 	}
-}
+}*/
